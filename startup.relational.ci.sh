1f1959e3fac10353aa7a8f4a0e2121c985279741
ThanhSon00 <phanson999999@gmail.com>
2025-07-02 17:01:37 +0700

    fix(CI): remove prod log export

--- startup.relational.ci.sh
+++ startup.relational.ci.sh
@@ -3,8 +3,7 @@ set -e
 
 /opt/wait-for-it.sh postgres:5432
 npm run migration:run
-# npm run seed:run:relational
-npm run start:prod > prod.log 2>&1 &
+npm run start:prod
 /opt/wait-for-it.sh maildev:1080
 /opt/wait-for-it.sh localhost:3000
 npm run lint
